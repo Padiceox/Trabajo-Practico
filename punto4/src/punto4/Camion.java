b27d3632c0f9b3833c0bd0e3f1e47fe9da4c7983
Padiceox <nahuelabogo@gmail.com>
2017-06-07 10:12:00 -0300

    Update Camion.java

--- punto4/src/punto4/Camion.java
+++ punto4/src/punto4/Camion.java
@@ -3,13 +3,13 @@ package punto4;
 public class Camion extends Vehiculo { //Herencia hija
 	// Creamos un constructor
 	public Camion(){
-	this.marca = "Mercedes Benz";
-	this.asientos = 2;
-	this.color = "Rojo";
+	this.marca = "Ford";
+	this.asientos = 4;
+	this.color = "Azul";
 	}
 	public Camion(String marca){ //Polimorfismo
 		this.marca  = marca;
-		this.asientos = 2;
-		this.color = "Rojo";
+		this.asientos = 4;
+		this.color = "Azul";
 	}
 }
